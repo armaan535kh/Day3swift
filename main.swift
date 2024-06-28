3267d320436249fcea8f48b13b59a81044c725fe
Armaan Khan <kh.armaan535@gmail.com>
2024-06-28 05:05:39 +0000

    this is my day 3 code learning swift

--- main.swift
+++ main.swift
@@ -5,32 +5,32 @@
 
 // Write a Swift program that declares two variables, performs basic arithmetic operations on them (addition, subtraction, multiplication, and division), and prints the result.
 
-// print("Enter the first number: ", terminator: "")
-// var aNum = Int(readLine()!)!
+print("Enter the first number: ", terminator: "")
+var aNum = Int(readLine()!)!
 
-// print("Enter the second number: ", terminator: "")
-// var bNum = Int(readLine()!)!
+print("Enter the second number: ", terminator: "")
+var bNum = Int(readLine()!)!
 
-// var sum = aNum + bNum 
-// var diff = aNum - bNum
-// var div = aNum / bNum
-// var mul = aNum * bNum 
+var sum = aNum + bNum 
+var diff = aNum - bNum
+var div = aNum / bNum
+var mul = aNum * bNum 
 
-// print("The sum of \(aNum)", "substraction is \(diff)", "multiplication is \(mul)", "Division is \(div)", separator: " and ")
+print("The sum of \(aNum)", "substraction is \(diff)", "multiplication is \(mul)", "Division is \(div)", separator: " and ")
 
 
 // Exercise 2: Compound Expressions
 
 // Create a Swift program that calculates the area and circumference of a circle given its radius. Use expressions to perform the calculations and print the results.
-// import Foundation
+import Foundation
 
-// let radius = 5.0
+let radius = 5.0
 
-// let area = Double.pi * pow(radius, 2)
+let area = Double.pi * pow(radius, 2)
 
-// let circumfrence = 2 * Double.pi * radius 
+let circumfrence = 2 * Double.pi * radius 
 
-// print( " Radius is \(radius)", "Area is \(area)", separator: " and ")
+print( " Radius is \(radius)", "Area is \(area)", separator: " and ")
 
 // Exercise 3: Conditional Statements
 
@@ -41,17 +41,17 @@
 //     Prints a message indicating whether the number is positive, negative, or zero.
 
 
-// print("Enter the number: ", terminator: "")
-// var num = Int(readLine()!)!
-// if num > 0 {
-//   print("The number is positive")
-// }
-// else if num < 0 {
-//   print("The number is negative")
-// }
-// else if num == 0 {
-//   print("The number is zero")
-// }
+print("Enter the number: ", terminator: "")
+var num = Int(readLine()!)!
+if num > 0 {
+  print("The number is positive")
+}
+else if num < 0 {
+  print("The number is negative")
+}
+else if num == 0 {
+  print("The number is zero")
+}
 
 // Exercise 4: Nested Statements
 
@@ -62,16 +62,16 @@
 //     If the number is even, check if it is also divisible by 4.
 //     Print appropriate messages for each condition.
 
-// print("Enter the number : ", terminator: "")
-// var num = Int(readLine()!)!
+print("Enter the number : ", terminator: "")
+var num = Int(readLine()!)!
 
-// if num % 2 == 0 {
-//   print("The number is even")
+if num % 2 == 0 {
+  print("The number is even")
 
-//   if num % 4 == 0 {
-//     print("The number is divisible by 4")
-//   }
-// }
+  if num % 4 == 0 {
+    print("The number is divisible by 4")
+  }
+}
 
 
 // Exercise 5: Code Blocks
@@ -86,24 +86,24 @@
 //         Check if the score is greater than or equal to 60 but less than 70, and if so, print "Grade: D".
 //         If the score is less than 60, print "Grade: F".
 
-// print("Enter your scoe: ", terminator: "")
-// var score = Int(readLine()!)!
+print("Enter your scoe: ", terminator: "")
+var score = Int(readLine()!)!
 
-// if score >= 90 {
-//   print("Grade A")
-// }
-// else if score >= 80 && score < 90 {
-//   print("Grade B")
-// }
-// else if score >= 70 && score < 80 {
-//   print("Grade C")
-// }
-// else if score >= 60 && score < 70 {
-//   print("Grade D")
-// }
-// else if score < 60 {
-//   print("Grade F")
-// } 
+if score >= 90 {
+  print("Grade A")
+}
+else if score >= 80 && score < 90 {
+  print("Grade B")
+}
+else if score >= 70 && score < 80 {
+  print("Grade C")
+}
+else if score >= 60 && score < 70 {
+  print("Grade D")
+}
+else if score < 60 {
+  print("Grade F")
+} 
 
 // Exercise 6: Combining Expressions and Code Blocks
 
